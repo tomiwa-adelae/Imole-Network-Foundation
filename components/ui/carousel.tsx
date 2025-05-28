b67a494cdcf2960c3031f3e34e3b3c6ed433e8ad
Tomiwa Adelae <thetommedia@gmail.com>
2025-05-28 19:53:33 +0100

    Fixed an error

--- components/ui/carousel.tsx
+++ components/ui/carousel.tsx
@@ -241,7 +241,7 @@ const Slide = ({ slide, index, current, handleSlideClick }: SlideProps) => {
 
 	const xRef = useRef(0);
 	const yRef = useRef(0);
-	const frameRef = useRef<number>();
+	const frameRef = useRef<number | null>(null);
 
 	useEffect(() => {
 		const animate = () => {
@@ -259,7 +259,7 @@ const Slide = ({ slide, index, current, handleSlideClick }: SlideProps) => {
 		frameRef.current = requestAnimationFrame(animate);
 
 		return () => {
-			if (frameRef.current) {
+			if (frameRef.current !== null) {
 				cancelAnimationFrame(frameRef.current);
 			}
 		};
