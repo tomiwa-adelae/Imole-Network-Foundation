0bcb931f50636cf2a872cdd779ec0e91872c9000
Tomiwa Adelae <thetommedia@gmail.com>
2025-06-12 19:46:26 +0100

    Fixed an error

--- components/Blogs.tsx
+++ components/Blogs.tsx
@@ -8,13 +8,13 @@ export const Blogs = () => {
 	return (
 		<div className="py-16">
 			<div className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-				{latestNews.map(({ image, title, excerpt, id }, index) => (
+				{latestNews.map(({ image, title, overview, id }, index) => (
 					<BlogBox
 						id={id}
 						key={index}
 						title={title}
-						excerpt={excerpt}
-						image={image}
+						overview={overview}
+						image={image[0]}
 					/>
 				))}
 			</div>
